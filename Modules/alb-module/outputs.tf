a59436bace57df59d435fa60105e02c9c72ae965
pgr-automation <grprashanth94@gmail.com>
2024-11-21 00:30:57 +0530

    terraform

--- Modules/alb-module/outputs.tf
+++ Modules/alb-module/outputs.tf
@@ -1,12 +1,12 @@
 output "target_group_arn" {
-    value = aws_lb_target_group.pgr-alb-tg.arn
+    value = aws_lb_target_group.alb-tg.arn
   
 }
 
 output "alb_dns_name" {
-  value = aws_lb.pgr-alb.dns_name
+  value = aws_lb.alb.dns_name
 }
 
 output "alb_arn" {
-  value = aws_lb.pgr-alb.arn
+  value = aws_lb.alb.arn
 }
