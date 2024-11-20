ff26e9defa0bcbb90de5f27745ba15aab828661b
pgr-automation <grprashanth94@gmail.com>
2024-11-20 19:42:06 +0530

    terraform

---
+++ Modules/alb-securityGroup-module/variables.tf
@@ -0,0 +1,9 @@
+variable "alb_sg_name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "asg_sg_name" {
+  description = "Name of the security group"
+  type        = string
+}
