e4ad3254b7c480a2138ee05b47b41badc2c58f0f
pgr-automation <grprashanth94@gmail.com>
2025-01-04 15:37:53 +0530

    Stable Version 1.0.0

--- Modules/vpc-module/aws/variables.tf
+++ Modules/vpc-module/aws/variables.tf
@@ -73,3 +73,15 @@ variable "aws_nat_gateway_name" {
   default = "nat-gw"
   
 }
+variable "enable_dns_hostnames" {
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC"
+  type        = bool
+  default     = ""
+  
+}
+variable "enable_dns_support" {
+  description = "A boolean flag to enable/disable DNS support in the VPC"
+  type        = bool
+  default     = ""
+  
+}
