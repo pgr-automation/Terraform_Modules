4b30686b3ad44f9e8975261293c5a7acbef6cf47
pgr-automation <grprashanth94@gmail.com>
2025-01-11 17:44:32 +0530

    vpc

--- Modules/vpc-module/aws/variables.tf
+++ Modules/vpc-module/aws/variables.tf
@@ -76,12 +76,13 @@ variable "aws_nat_gateway_name" {
 variable "enable_dns_hostnames" {
   description = "A boolean flag to enable/disable DNS hostnames in the VPC"
   type        = bool
-  default     = ""
+  default = false
+  
   
 }
 variable "enable_dns_support" {
   description = "A boolean flag to enable/disable DNS support in the VPC"
   type        = bool
-  default     = ""
+  default     = false
   
 }
--- Modules/vpc-module/vpc_main.tf
+++ Modules/vpc-module/vpc_main.tf
@@ -1,11 +1,21 @@
-module "vpc" {
-  source = "./aws"
-  cidr_block = "12.0.0.0/16"
-  vpc_name = "pgr-vpc"
-  internet_gateway_name = "pgr-igw"
-  public_subnet_cidrs = ["12.0.1.0/24"]
-  public_subnet_name = "pgr-public-subnet"
-  private_subnet_cidrs = ["12.0.2.0/24", "12.0.3.0/24"]
-  private_subnet_name = "pgr-private-subnet"
-
+provider "aws" {
+    region = "us-east-1"
+  
 }
+
+module "vpc" {
+  source                     = "github.com/pgr-automation/Terraform_Modules/Modules/vpc-module/aws"
+  vpc_cidr                   = "12.0.0.0/16"
+  public_subnet_cidrs        = ["12.0.1.0/24", "12.0.2.0/24"]
+  private_subnet_cidrs       = ["12.0.3.0/24", "12.0.4.0/24"]
+  region                     = "us-east-1"
+  public_subnet_name_prefix  = "pgr-public-subnet"   # Custom name prefix
+  private_subnet_name_prefix = "pgr-private-subnet"
+  availability_zones         = ["us-east-1a", "us-east-1b"]  # Specific availability zones
+  internet_gateway_name      = "pgr-igw"  # Custom Internet Gateway name
+  nat_elastic_ip_name        = "pgr-nat-eip"
+  aws_nat_gateway_name       = "pgr-nat-gw"
+  public_route_table_name    = "pgr-pub-rt"
+  private_route_table_name   = "pgr-private-rt"
+}
+
