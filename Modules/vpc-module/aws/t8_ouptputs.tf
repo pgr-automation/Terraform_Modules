466b6dfee2418e6124aaeff18fb596df9c483c38
pgr-automation <grprashanth94@gmail.com>
2025-02-03 13:33:31 +0530

    vpc

--- Modules/vpc-module/aws/t8_ouptputs.tf
+++ Modules/vpc-module/aws/t8_ouptputs.tf
@@ -1,41 +1,23 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main.id
+output "network_details" {
+  description = "Network resource details"
+  value = {
+    vpc = {
+      id = aws_vpc.main.id
+    }
+    internet_gateway = {
+      id = aws_internet_gateway.igw.id
+    }
+    subnets = {
+      public  = [for subnet in aws_subnet.public : subnet.id]
+      private = [for subnet in aws_subnet.private : subnet.id]
+    }
+    nat_gateway = {
+      id  = aws_nat_gateway.nat-gw.id
+      eip = aws_eip.nat_elastic_ip.public_ip
+    }
+    route_tables = {
+      public  = aws_route_table.public.id
+      private = aws_route_table.private.id
+    }
+  }
 }
-
-output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
-  value       = aws_internet_gateway.igw.id
-}
-
-output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
-}
-
-output "nat_gateway_id" {
-  description = "The ID of the NAT Gateway"
-  value       = aws_nat_gateway.nat-gw.id
-}
-
-output "nat_gateway_eip" {
-  description = "Elastic IP of the NAT Gateway"
-  value       = aws_eip.nat_elastic_ip.public_ip
-}
-
-output "public_route_table_id" {
-  description = "The ID of the public route table"
-  value       = aws_route_table.public.id
-}
-
-output "private_route_table_id" {
-  description = "The ID of the private route table"
-  value       = aws_route_table.private.id
-  # 
-}
-
