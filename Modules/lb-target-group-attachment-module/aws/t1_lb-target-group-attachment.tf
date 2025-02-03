bc7082b058ecd6c8748f2510a4413f33b5d73b7a
pgr-automation <grprashanth94@gmail.com>
2025-02-04 02:24:45 +0530

    target group

--- Modules/lb-target-group-attachment-module/aws/t1_lb-target-group-attachment.tf
+++ Modules/lb-target-group-attachment-module/aws/t1_lb-target-group-attachment.tf
@@ -1,4 +1,4 @@
-resource "aws_lb_target_group_attachment" "name" {
+resource "aws_lb_target_group_attachment" "this" {
   name = var.name
   target_group_arn = var.target_group_arn
   target_id        = var.target_id
