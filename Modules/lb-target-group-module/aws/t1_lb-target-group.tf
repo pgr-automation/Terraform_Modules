a836c9b8bb7567b79ac27a1cbdc2e09db60cc43b
pgr-automation <grprashanth94@gmail.com>
2025-02-04 01:53:19 +0530

    target group

---
+++ Modules/lb-target-group-module/aws/t1_lb-target-group.tf
@@ -0,0 +1,24 @@
+resource "aws_lb_target_group" "this" {
+  name        = var.name
+  port        = var.port
+  protocol    = var.protocol
+  vpc_id      = var.vpc_id
+  target_type = var.target_type
+  load_balancing_algorithm_type = var.load_balancing_algorithm_type
+
+  health_check {
+    enabled             = var.health_check_enabled
+    interval            = var.health_check_interval
+    path                = var.health_check_path
+    port                = var.health_check_port
+    protocol            = var.health_check_protocol
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  
+}
---
+++ Modules/lb-target-group-module/aws/variables.tf
@@ -0,0 +1,69 @@
+variable "name" {
+  description = "The name of the target group"
+  type        = string
+  
+}
+variable "port" {
+  description = "The port on which targets receive traffic"
+  type        = number
+  
+}
+variable "protocol" {
+  description = "The protocol to use for routing traffic to the targets"
+  type        = string
+  default     = "HTTP"
+}
+variable "vpc_id" {
+  description = "The identifier of the VPC in which to create the target group"
+  type        = string
+}
+variable "target_type" {
+  description = "The type of target that you must specify when registering targets with this target group"
+  type        = string
+  default     = "instance"
+}
+variable "load_balancing_algorithm_type" {
+  description = "The load balancing algorithm determines how the load balancer selects targets when routing requests"
+  type        = string
+  default     = "round_robin"
+}
+variable "health_check_enabled" {
+  description = "Indicates whether health checks are enabled"
+  type        = bool
+  default     = true
+}
+variable "health_check_interval" {
+  description = "The approximate amount of time, in seconds, between health checks of an individual target"
+  type        = number
+  default     = 30
+}
+variable "health_check_path" {
+  description = "The destination for the health check request"
+  type        = string
+  default     = "/"
+}
+variable "health_check_port" {
+  description = "The port to use to connect with the target"
+  type        = string
+  default     = "traffic-port"
+}
+variable "health_check_protocol" {
+  description = "The protocol to use to connect with the target"
+  type        = string
+  default     = "HTTP"
+}
+variable "health_check_timeout" {
+  description = "The amount of time, in seconds, during which no response means a failed health check"
+  type        = number
+  default     = 5
+}
+variable "health_check_healthy_threshold" {
+  description = "The number of consecutive health checks successes required before considering an unhealthy target healthy"
+  type        = number
+  default     = 5
+}
+variable "health_check_unhealthy_threshold" {
+  description = "The number of consecutive health check failures required before considering the target unhealthy"
+  type        = number
+  default     = 2
+}
